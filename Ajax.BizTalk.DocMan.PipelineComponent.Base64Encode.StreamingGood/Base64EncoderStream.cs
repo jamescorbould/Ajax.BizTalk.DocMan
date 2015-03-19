9f1c4346f330579a3732842521149806071ec608
James Corbould <james.bron@hotmail.com>
2015-03-20 07:35:39 +1300

    Updated Read method on base64 encoder stream.

--- Ajax.BizTalk.DocMan.PipelineComponent.Base64Encode.StreamingGood/Base64EncoderStream.cs
+++ Ajax.BizTalk.DocMan.PipelineComponent.Base64Encode.StreamingGood/Base64EncoderStream.cs
@@ -13,6 +13,10 @@ namespace Ajax.BizTalk.DocMan.PipelineComponent
     {
         private Guid callToken { get; set; }
         private VirtualStream s { get; set; }
+        private byte[] bufferedBytes { get; set; }
+        private int bufferedBytesCount { get; set; }
+        private int bytesNumCopiedAlready { get; set; }
+        private const int BUFFER_SIZE = 4096;
         public override long Position { get; set; }
         public override long Length { get { return this.s.Length; } }
         public override bool CanWrite { get { return false; } }
@@ -24,6 +28,9 @@ namespace Ajax.BizTalk.DocMan.PipelineComponent
             this.s = new VirtualStream(VirtualStream.MemoryFlag.AutoOverFlowToDisk);
             s.CopyTo(this.s);
             this.callToken = TraceManager.CustomComponent.TraceIn();
+            bufferedBytes = new byte[0];
+            bytesNumCopiedAlready = 0;
+            bufferedBytesCount = 0;
 
             TraceManager.CustomComponent.TraceInfo(string.Format("{0} - {1} - Called constructor on Base64EncoderStream class.", System.DateTime.Now, this.callToken));
         }
@@ -32,12 +39,38 @@ namespace Ajax.BizTalk.DocMan.PipelineComponent
         {
             TraceManager.CustomComponent.TraceInfo(string.Format("{0} - {1} - Called Read method on Base64EncoderStream class.", System.DateTime.Now, this.callToken));
 
-            string base64 = Convert.ToBase64String(buffer);
-            byte[] bytes = Convert.FromBase64String(base64);
+            var countBytesRead = this.s.Read(buffer, offset, count);
 
-            var bytesread = this.s.Read(bytes, 0, bytes.Length);
+            byte[] bytesRead = new byte[countBytesRead];
 
-            return bytesread;
+            Array.Copy(buffer, bytesRead, countBytesRead);
+
+            string base64 = Convert.ToBase64String(bytesRead);
+            byte[] base64EncodedAsBytes = System.Text.Encoding.ASCII.GetBytes(base64);
+
+            int encodedBytesCount = base64EncodedAsBytes.Count();
+
+            if (encodedBytesCount > BUFFER_SIZE)
+            {
+                int countOfOverflowBytes = (base64EncodedAsBytes.Count() - BUFFER_SIZE);
+
+                byte[] tempBytes = new byte[this.bufferedBytes.Count() + countOfOverflowBytes];
+
+                Array.Copy(bufferedBytes, tempBytes, this.bufferedBytes.Count());
+
+                this.bufferedBytes = new byte[bufferedBytesCount + countOfOverflowBytes];
+
+                Array.Copy(tempBytes, this.bufferedBytes, tempBytes.Count());
+                Array.Copy(base64EncodedAsBytes, BUFFER_SIZE, this.bufferedBytes, tempBytes.Count(), countOfOverflowBytes);
+
+                bufferedBytesCount = this.bufferedBytes.Count();
+
+                encodedBytesCount = BUFFER_SIZE;
+            }
+
+            Array.Copy(base64EncodedAsBytes, 0, buffer, 0, encodedBytesCount);
+
+            return encodedBytesCount;
         }
 
         public override void SetLength(long value)
